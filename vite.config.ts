edf7605546a57db1a5f9a0cabab9634e7ac6e567
Vince Schramer <885196+scvnc@users.noreply.github.com>
2023-04-14 16:34:31 -0500

    fix baseurl for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/experiment-primevue-shadowdom/',
   plugins: [vue()],
   resolve: {
     alias: {
